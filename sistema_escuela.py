1f651a8e5c51ce48d5bc9858851a8eba023a86dc
Brayan Ruiz <brayanja00@hotmail.com>
2020-05-13 22:21:02 -0500

    Ampliación, Fase 3

--- sistema_escuela.py
+++ sistema_escuela.py
@@ -1,58 +1,145 @@
+import sys
+import sqlite3
+from sqlite3 import Error
 import pandas as pd
 
+# Creación de la base de datos en SQLite3
+try:
+    with sqlite3.connect("Escuela.db") as conn:
+        c = conn.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS Alumno (Matricula INTEGER PRIMARY KEY, Nombre TEXT NOT NULL);")
+        c.execute("CREATE TABLE IF NOT EXISTS Asignatura (ID_Asignatura INTEGER PRIMARY KEY, Materia TEXT NOT NULL);")
+        c.execute("CREATE TABLE IF NOT EXISTS Calificaciones (Matricula INTEGER, ID_Asignatura INTEGER, Calificacion INTEGER, FOREIGN KEY (Matricula) REFERENCES Alumno(Matricula), FOREIGN KEY (ID_Asignatura) REFERENCES Asignatura(ID_Asignatura));")
+        print("Tablas creadas exitosamente! \n")
+except Error as e:
+    print(e)
+except:
+    print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+
+# Guardar asignaturas a la base de datos
+try:
+    with sqlite3.connect("Escuela.db") as conn:
+        c = conn.cursor()
+        c.execute("INSERT INTO Asignatura VALUES(1, 'Programacion');")
+        c.execute("INSERT INTO Asignatura VALUES(2, 'Contabilidad');")
+        c.execute("INSERT INTO Asignatura VALUES(3, 'Redes');")
+        c.execute("INSERT INTO Asignatura VALUES(4, 'Economia');")
+        c.execute("INSERT INTO Asignatura VALUES(5, 'Ingles');")
+except Error as e:
+    print(e)
+except:
+    print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+
+# Funciones de inserción de datos
+def guardar_alumnos(matricula, nombre):
+    try:
+        with sqlite3.connect("Escuela.db") as conn:
+            c = conn.cursor()
+            valores = {"Matricula":matricula, "Nombre":nombre}
+            c.execute("INSERT INTO Alumno VALUES(:Matricula,:Nombre)", valores)
+    except Error as e:
+        print(e)
+    except:
+        print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+
+def guardar_notas(matricula, clave, cal):
+    try:
+        with sqlite3.connect("Escuela.db") as conn:
+            c = conn.cursor()
+            valores = {"Matricula":matricula, "Clave":clave, "Cal":cal}
+            c.execute("INSERT INTO Calificaciones VALUES(:Matricula,:Clave,:Cal)", valores)                                                                                                 #a
+    except Error as e:
+        print(e)
+    except:
+        print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+
+# Inicio del programa
+
+def menu_principal():
+    print("\n- MENÚ DEL SISTEMA -")
+    print("[1] Agregar alumnos")
+    print("[2] Ingresar calificaciones")
+    print("[3] Estadisticos descriptivos")
+    print("[4] Asignaturas reprobadas")
+    print("[5] Reporte de calificaciones")
+    print("[6] Exportación")
+    print("[7] Consulta SQL con JOINS")
+    print("[9] Salir")
+    
 diccionario_alumnos = {}
 diccionario_calificaciones = {}
 
-i = 1
-while i > 0:
-    opcion = int(input("- MENÚ DEL SISTEMA - \n [1] Agregar alumnos \n [2] Ingresar calificaciones \n [3] Estadisticos descriptivos \n [4] Estudiantes Reprobados \n [5] Ver las calificaciones de los alumnos \n [6] Exportación \n \n Eliga el número de la opción que desee: "))
+ciclo = True
+while ciclo:
+    continuar = True
+    menu_principal()
+    opcion = int(input("Eliga el número de la opción que desee: "))
+    # opcion = int(input("- MENÚ DEL SISTEMA - \n [1] Agregar alumnos \n [2] Ingresar calificaciones \n [3] Estadisticos descriptivos \n [4] Estudiantes Reprobados \n [5] Ver las calificaciones de los alumnos \n [6] Exportación \n \n Eliga el número de la opción que desee: "))
     
-    if opcion == 1: 
+    if opcion == 1:
         
-        clave = int(input("Ingrese la matricula del nuevo estudiante: "))
-        nombre = input("Ingrese el nombre del nuevo estudiante: ")
-        
-        if clave not in (diccionario_alumnos.keys()):
-            diccionario_alumnos[clave] = [nombre]
-            print("\n -- Alumno agregado -- \n")
-        else:
-            print("\n Esa matricula YA EXISTE, no se permiten matriculas duplicadas.")
-            print("Esta es la lista de alumnos registrados:")
+        while continuar:
+            print("Proporcione los datos de los alumnos, capture la clave 0 (cero) para terminar.")
             
-        print(diccionario_alumnos)
-        print("\n")
+            matricula = int(input("Ingrese la matricula del nuevo estudiante: "))
+            
+            if matricula == 0:
+                continuar = False
+            elif matricula not in (diccionario_alumnos.keys()):
+                nombre = input("Ingrese el nombre del nuevo estudiante: ")
+                guardar_alumnos(matricula, nombre)
+                
+                diccionario_alumnos[matricula] = [nombre]
+                print("\n -- Alumno agregado -- \n")
+                print(diccionario_alumnos)
+                print("")
+            else:
+                print("\n Esa matricula YA EXISTE, no se permiten matriculas duplicadas.")
+                print("Esta es la lista de alumnos registrados: ")
     
     elif opcion == 2:
         
-        matricula = int(input("Ingresa la matricula del estudiante a calificar: "))
-        name = (input("Ingresa el nombre del estudiante a calificar: "))
-        
-        if matricula in (diccionario_alumnos.keys()):
-            cal1 = int(input("Ingrese la calificacion de Programacion: "))
+        while continuar:
+            print("Proporcione las calificaciones de cada alumno, capture la clave 0 (cero) para terminar.")
+            
+            matricula = int(input("Ingresa la matricula del estudiante a calificar: "))
+            
+            if matricula == 0:
+                continuar = False
+            elif matricula in (diccionario_alumnos.keys()):
+                name = (input("Ingresa el nombre del estudiante a calificar: "))
+                
+                cal1 = int(input("Ingrese la calificacion de Programacion: "))
 
-            cal2 = int(input("Ingrese la calificacion de Contabilidad: "))
+                cal2 = int(input("Ingrese la calificacion de Contabilidad: "))
 
-            cal3 = int(input("Ingrese la calificacion de Base de Datos: "))
+                cal3 = int(input("Ingrese la calificacion de Redes: "))
 
-            cal4 = int(input("Ingrese la calificacion de Economia: "))
+                cal4 = int(input("Ingrese la calificacion de Economia: "))
 
-            cal5 = int(input("Ingrese la calificacion de Ingles: "))
-            if cal1 <= 100 and cal2 <= 100 and cal3 <= 100 \
-               and cal4 <= 100 and cal5 <= 100:
-                diccionario_calificaciones[matricula] = [[name], cal1, cal2, cal3, cal4, cal5]
+                cal5 = int(input("Ingrese la calificacion de Ingles: "))
+                if cal1 <= 100 and cal2 <= 100 and cal3 <= 100 \
+                   and cal4 <= 100 and cal5 <= 100:
+                    guardar_notas(matricula, 1, cal1)
+                    guardar_notas(matricula, 2, cal2)
+                    guardar_notas(matricula, 3, cal3)
+                    guardar_notas(matricula, 4, cal4)
+                    guardar_notas(matricula, 5, cal5)
+                    
+                    diccionario_calificaciones[matricula] = [name, cal1, cal2, cal3, cal4, cal5]
+                else:
+                    print("\n -- Ingresaste una calificacion invalida -- \n")
             else:
-                print("Ingresaste una calificacion invalida")
-        else:
-            print("Ese alumno no existe en el registro \n")
-        print("Lista de alumnos: \n")
-        print(diccionario_alumnos)
-        print()
+                print("Ese alumno no existe en el registro \n")
+            print("\n Lista de alumnos: \n")
+            print(diccionario_alumnos)
+            print("")
     
     elif opcion == 3:
         
         try:
             notas_alumnos = pd.DataFrame(diccionario_calificaciones)
-            notas_alumnos.index = ["Nombre", "Programacion", "Contabilidad", "Base de Datos", "Economia", "Ingles"]
+            notas_alumnos.index = ["Nombre", "Programacion", "Contabilidad", "Redes", "Economia", "Ingles"]
             
             eleccion = int(input("\n 1)- Asignaturas \n 2)- Alumnos \n Eliga una opción: "))
             
@@ -131,10 +218,32 @@ while i > 0:
             print("\n Procederemos a crear el archivo JSON....")
             notas_alumnos.T.to_json('notas_alumnos.json', orient= "table")
             print("-- Exportado -- \n")
+    
+    elif opcion == 7:
+        try:
+            with sqlite3.connect("Escuela.db") as conn:
+                c = conn.cursor()
+                c.execute("SELECT Nombre, Materia, Calificacion FROM Calificaciones INNER JOIN Alumno ON Calificaciones.Matricula = Alumno.Matricula INNER JOIN Asignatura ON Calificaciones.ID_Asignatura = Asignatura.ID_Asignatura;")
+                registros = c.fetchall()
             
+            for registro in registros:
+                print(registro)
+        except Error as e:
+            print(e)
+        except:
+            print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+            
+    elif opcion == 9:
+        ciclo = False
+    else:
+        print(f"La opción {opcion} no es valida, asegurese de capturar una opción numerica. \n")
+        
+print("PROGRAMA CONCLUIDO")
+        
             
         
         
     
         
+
 
